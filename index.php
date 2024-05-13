f3d56d892c133b56413a14a9eed8e2281bbc756a
rakhagans37 <muhammadrakha3704@gmail.com>
2024-05-13 19:57:55 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="./src/output.css" rel="stylesheet" />
+</head>
+
+<body>
+    <p id="text"></p>
+</body>
+
+<script>
+    async function getAllData() {
+        const url =
+            "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+
+        const header = {
+            Accept: "application/json",
+            "X_CMC-PRO-API-KEY": "0ad0fd50-4b81-4477-8bd2-2428072a6f2a",
+        };
+
+        let data = await fetch(url, {
+            method: "GET",
+            headers: header
+        });
+        data = await data.json();
+        return data;
+    }
+
+    getAllData().then((data) => {
+        console.log(data);
+        document.getElementById("text").innerHTML = JSON.stringify(data);
+    });
+</script>
+
+</html>
