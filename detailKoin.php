d6e8df994a6a0b36dba7e617fb4cf52397a015c9
rakhagans37 <muhammadrakha3704@gmail.com>
2024-06-02 19:57:08 +0700

    Perbaiki Error

--- detailKoin.php
+++ detailKoin.php
@@ -3,7 +3,8 @@
 
 <head>
     <title>Crypo - Pantauan Crypto</title>
-    <meta charset="UTF-8" / <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="./src/output.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600;700&display=swap" rel="stylesheet">
