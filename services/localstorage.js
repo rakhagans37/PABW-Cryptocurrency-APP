31b64be9bdc58c552798b8060f749cfcd083192b
rakhagans37 <muhammadrakha3704@gmail.com>
2024-06-05 01:41:19 +0700

    done

--- services/localstorage.js
+++ services/localstorage.js
@@ -1,15 +1,3 @@
-function saveCoin(id) {
-    let coins = JSON.parse(localStorage.getItem("coins")) || [];
-    coins.push(id);
-    localStorage.setItem("coins", JSON.stringify(coins));
-}
-
-function removeCoin(id) {
-    let coins = JSON.parse(localStorage.getItem("coins")) || [];
-    coins = coins.filter((coin) => coin !== id);
-    localStorage.setItem("coins", JSON.stringify(coins));
-}
-
 export function addToWatchlist(...id) {
     const watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
     watchlist.push(...id);
@@ -17,9 +5,14 @@ export function addToWatchlist(...id) {
 }
 
 export function removeFromWatchlist(...id) {
-    let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
-    watchlist = watchlist.filter((coin) => !id.includes(coin));
-    localStorage.setItem("watchlist", JSON.stringify(watchlist));
+    try {
+        let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+        watchlist = watchlist.filter((coin) => !id.includes(coin));
+        localStorage.setItem("watchlist", JSON.stringify(watchlist));
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
 
 export function buyCoin(id, totalCoin, buyPrice) {
